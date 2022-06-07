f70bba659d81bd46efc00e11e0d4959daff80bcf
musha-SES <musha@se-sendai.co.jp>
2022-06-07 11:10:02 +0900

    textedit

--- 一言掲示板_test/property.php
+++ 一言掲示板_test/property.php
@@ -48,14 +48,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') { //ブラウザのリクエストが
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>設定</title>
+    <title>ヒトコト</title>
 
     <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
     <div id="wrap">
         <div id="head">
-            <h1>Property</h1> 
+            <h1>ヒトコト --Property--</h1> 
         </div>
         <div id="content">
             <div class="titlebar">
