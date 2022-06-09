e3151f8a1df13e5b469d31d301982bfb78279929
musha-SES <musha@se-sendai.co.jp>
2022-06-09 17:41:11 +0900

    add text

--- BB/withdrawal.php
+++ BB/withdrawal.php
@@ -11,22 +11,30 @@ require('library.php');
     
     $db = dbconnect(); //DB接続
 
-    /* 退会処理 */
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    /* ログイン状態で、かつ退会ボタンを押した */
-    if (isset($_SESSION['name']) && isset($_POST['is_delete']) && $_POST['is_delete'] === '1') {
-        /* 退会 */
-        $stmt = $db->prepare('DELETE m,p,pl FROM members m LEFT OUTER JOIN posts p ON p.id = m.id LEFT OUTER JOIN profile pl ON p.id = m.id WHERE m.id = ?');
-        $stmt->bind_param('i', $id);
-        $stmt->execute();
+    // 退会処理 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // ログイン状態で、かつ退会ボタンを押した 
+        if (isset($_SESSION['name']) && isset($_POST['is_delete']) && $_POST['is_delete'] === '1') {
+            /* 退会 */
+            $stmt = $db->prepare('DELETE m,p,pl FROM members m LEFT OUTER JOIN posts p ON p.id = m.id LEFT OUTER JOIN profile pl ON p.id = m.id WHERE m.id = ?');
+            $stmt->bind_param('i', $id);
+            $stmt->execute();
 
-        session_destroy(); // セッションを破壊
+            session_destroy(); // セッションを破壊
 
-        header('Location: ./login.php');
-        exit; 
+            header('Location: ./login.php');
+            exit; 
+        }
     }
-}
 ?>
+
+<script>
+//確認ホップアップ表示
+function EndUser(){
+    var EN = confirm('本当に退会してよろしいですか？');
+    return EN;
+}
+</script>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -43,14 +51,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <h1>ヒトコト --退会手続き--</h1> 
             </div>
             <div id="content">
+
                 <div class="titlebar">
                     <p class="subject">&laquo<a href="property.php">property</a></p>
                 </div>
-                    <p>退会しますか？</p>
-                    <form method="POST">
+                <div>
+                    <dt><h5>退会しますか？</h5></dt>
+                    <form method="POST" onsubmit="return EndUser()">
                         <input type="hidden" name="is_delete" value="1">
                         <input type="submit" value="退会する">
                     </form>
+                </div>
             </div>
     </div>
 </body>
